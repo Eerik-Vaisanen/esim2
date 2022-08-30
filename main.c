2c85af6199feedc7f241de862f0d5d12c2f797b0
Pekka Alaluukas <pekka.alaluukas@gmail.com>
2022-08-30 11:25:34 +0300

    osoitteen käsittely

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    int x=55;
+    int y=45;
+    int imax=INT_MAX;
+    printf("Muuttujan x arvo=%d\n",x);
+    printf("Muuttujan x muistipaikan osoite=%d\n",&x);
+    printf("Muuttujan y arvo=%d\n",y);
+    printf("Muuttujan y muistipaikan osoite=%d\n",&y);
+
+    printf("Suurin kokonaisluku on %d\n",imax);
+
     return 0;
 }
